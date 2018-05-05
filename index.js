535221e18101f11928f7853e15110aad2245fdfe
Sean Logan Para <spara77@gmail.com>
2018-05-05 18:45:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var katzDeliLine = []
+
+function takeANumber(line, name) { 
+  return `Welcome, ${name}. You are number ${line.push(name)} in line.`
+}
+
+function nowServing(line) {
+   if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+    
+  }
+  return `Currently serving ${line.splice(0,1)}.`
+ 
+}
+
+function currentLine(line){
+  if (line.length === 0) {
+    return "The line is currently empty."
+  }
+  
+}
